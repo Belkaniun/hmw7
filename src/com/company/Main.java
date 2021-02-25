db87ae74c7c60336f6f8a454a9520c72c66eec53
Vova Maksutenko <paladinusvx@gmail.com>
2021-02-25 06:32:25 +0200

    Task 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,42 +2,26 @@ package com.company;
 
 public class Main {
 
+
+    //2) Найти количество счастливых билетиков на трамвай от 000001 до 999999
+    // (те у которых сумма первых 3 цифр равна сумме последних 3)
+
     public static void main(String[] args) {
-        //1) Электронные часы показывают время в формате от 00:00 до 23:59.
-        //
-        //Подсчитать сколько раз за сутки случается так, что слева от двоеточия показывается симметричная комбинация для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51).
+        int sum = 0;
+        for (int i = 000001; i <= 999999; i++) {
+            int number1 = i / 100000 % 10,
+                    number2 = i / 10000 % 10,
+                    number3 = i / 1000 % 10,
+                    number4 = i / 100 % 10,
+                    number5 = i / 10 % 10,
+                    number6 = i % 10;
 
-
-        System.out.println("Всего " + time() + " раз");
-    }
-
-    public static int time() {
-        String hour = "0";
-        String minute = "0";
-        int counter = 0;
-        for (int hrs = 0; hrs <= 24; hrs++) {
-            hour = "0";
-            for (int mnts = 0; mnts <= 60; mnts++) {
-                minute = "0";
-                if (hrs < 10) {
-                    hour = "0" + String.valueOf(hrs);
-                } else {
-                    hour = String.valueOf(hrs);
-                }
-                if (mnts < 10) {
-                    minute = "0" + String.valueOf(mnts);
-                } else {
-                    minute = String.valueOf(mnts);
-                }
-                StringBuilder tmp = new StringBuilder(hour);
-                if (tmp.reverse().toString().equals(minute.toString())) {
-                    System.out.println(hour + ":" + minute);
-                    counter++;
-                }
+            if (number1 + number2 + number3 == number4 + number5 + number6) {
+                sum++;
             }
-
         }
-        return counter;
+        System.out.println("Рулон билетов с номерами от 000001 до 999999 имеет " + sum + " счастливых билетов.");
     }
 }
+
 
